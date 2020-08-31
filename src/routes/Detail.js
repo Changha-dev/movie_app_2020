8c6b3336caf8a32975fc16bb93fd0859488c1797
Changha-dev <mentenseoul@gmail.com>
2020-08-31 23:12:13 +0900

    finish

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Detail.css';
 
 class Detail extends React.Component {
     componentDidMount() {
@@ -11,7 +12,22 @@ class Detail extends React.Component {
     render() {
         const { location} = this.props;
         if (location.state) {
-        return <span>{location.state.title}</span>;
+        return (
+        <div className="movie__detail">
+                <img src={location.state.poster}/><br></br>
+            <div className="movie__data">
+                <h3>{location.state.title}</h3>
+                <h5 className="movie__year">{location.state.year}</h5> 
+                <ul className="movie__genres">
+                {location.state.genres.map((genre, index) => {
+                    return <li key={index} className="movie__genre">{genre}</li>
+                })}
+                </ul>    
+                <p class="movie__summary">{location.state.summary}</p>
+        </div>
+        </div>);
+        
+        
         } else {
             return null;
         }
